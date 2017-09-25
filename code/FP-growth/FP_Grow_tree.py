1043faaa638780579c7e0495ce247e69d3dcd34a
YXWisaboy <437472167@qq.com>
2017-09-25 11:02:35 +0800

    添加了FP树的内容

---
+++ code/FP-growth/FP_Grow_tree.py
@@ -0,0 +1,21 @@
+#-*- coding:utf-8 –*-
+__author__ = 'Dodd'
+import fptree
+import tree
+
+class FP_Grow_tree:
+	def __init__(self,datas,a,support):
+		self.f=fptree.fptree(datas,support)
+		self.f.fp_tree()
+		self.f.tree=tree.tree(self.f.getRootTree(),self.f.headtable,support,a)
+		#print(a)
+		self.f.tree.FP_growth(self.f.headnode,self.f.headtable)
+		
+		pass
+	def printfrequent(self):
+		self.f.tree.printfrequent()
+		pass
+	
+	def printconfident(self,con):
+		self.f.tree.printconfident(con)
+		pass
